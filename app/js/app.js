608cd6f7ffca3fd958f9dfdd17ea2617bc788eb4
Satyadarshin Perry <s.Dog@Satyadarshin.com>
2019-06-19 23:51:56 +0100

    Added some foundation sass styles and experiemented with a delay on the row creation table of Hugo Award winners. Made progess on understanding for ... in loops, setTimeout, setInterval, and classLits.

--- app/js/app.js
+++ app/js/app.js
@@ -9,44 +9,37 @@ xhttp.onreadystatechange = function() {
 xhttp.open("GET", "data/hugo_award_novels.json", true);
 xhttp.send();
 
+const rowBuilder = (i, tableContainer) => {
+  console.log(i);
+  let winnerRow = document.createElement('tr');
+  let winnerYear = document.createElement('td');
+  winnerYear.textContent = `${i.year}`;
+  let winnerAuthor = document.createElement('td');
+  winnerAuthor.textContent = `${i.winner.author}, `;
+  let winnerTitle = document.createElement('td');
+  winnerTitle.textContent = `"${i.winner.title}"`;
+  winnerRow.appendChild(winnerYear);
+  winnerRow.appendChild(winnerAuthor);
+  winnerRow.appendChild(winnerTitle);
+  tableContainer.appendChild(winnerRow);
+  setTimeout(()=>{winnerRow.classList.add('test_class');}, 1000)
+}
+
 const each = ( hugos ) => { 
-  const tableContainer = document.querySelector('#outcome');
+  const theContainer = document.querySelector('#outcome');
   for ( const novels in hugos ) {
-    let theString = hugos[novels];
-    for ( text in theString ) { 
-      let winnerRow = document.createElement('tr');
-      let winnerYear = document.createElement('td');
-      winnerYear.textContent =`${theString[text].year}`;
-      let winnerAuthor = document.createElement('td');
-      winnerAuthor.textContent = `${theString[text].winner.author}, `;
-      let winnerTitle = document.createElement('td');
-      winnerTitle.textContent =  `"${theString[text].winner.title}"`;
-      winnerRow.appendChild(winnerYear);
-      winnerRow.appendChild(winnerAuthor);
-      winnerRow.appendChild(winnerTitle);
-      tableContainer.appendChild(winnerRow);
+    let theWinners = hugos[novels];
+    for (thisYear in theWinners ) { 
+      rowBuilder(theWinners[thisYear], theContainer);
+     // (function () {setInterval(rowBuilder, 500, theWinners[thisYear], theContainer);})()
+      // setInterval(rowBuilder, 5000, thisYear, theWinners, theContainer);
     }
-    // for ( text in theString ) { 
-    //   let listContent = document.createElement('li');
-    //   listContent.textContent =`Winner, ${theString[text].year}: ${theString[text].winner.author}, "${theString[text].winner.title}".`;
-    //   listContainer.appendChild(listContent);
-    // }
-  }
-};
-// .filter()
 
-// .map()
+    // for (var x = 0, ln = list.length; x < ln; x++) {
+    //   setTimeout(function(y) {    
+    //     console.log("%d => %d", y, list[y] += 10);
+    //   }, x * 500, x); // we're passing x
+    // // }
 
-// .find()
-
-// .forEach()
-
-
-
-// .some()
-
-// .every()
-
-// .reduce()
-
-// .includes()
+   }
+}
