205514e3c28d573df86426c55da37f8106413648
Satyadarshin Perry <s.Dog@Satyadarshin.com>
2019-08-06 19:55:06 +0100

    Basic animation works: rows are added to the table, fade with a css animation and delete with JS. Clunky execution needs refinement. very rudimentary page layout.

--- app/js/app.js
+++ app/js/app.js
@@ -10,36 +10,62 @@ xhttp.open("GET", "data/hugo_award_novels.json", true);
 xhttp.send();
 
 const rowBuilder = (i, tableContainer) => {
-  console.log(i);
   let winnerRow = document.createElement('tr');
   let winnerYear = document.createElement('td');
   winnerYear.textContent = `${i.year}`;
   let winnerAuthor = document.createElement('td');
-  winnerAuthor.textContent = `${i.winner.author}, `;
+  winnerAuthor.textContent = `${i.winner.author}`;
   let winnerTitle = document.createElement('td');
-  winnerTitle.textContent = `"${i.winner.title}"`;
+  winnerTitle.textContent = `${i.winner.title}`;
   winnerRow.appendChild(winnerYear);
   winnerRow.appendChild(winnerAuthor);
   winnerRow.appendChild(winnerTitle);
   tableContainer.appendChild(winnerRow);
-  setTimeout(()=>{winnerRow.classList.add('test_class');}, 1000)
+  winnerRow.classList.add('test_class');
+  setTimeout(()=>{ tableContainer.removeChild(winnerRow)}, 1000)
+  // tableContainer.removeChild(winnerRow);
+}
+
+//This function is on the backburner. 
+//It should be called once, before the function to chug over the rows 
+const generateTableHead = (table, data) => {
+  let thead = table.createTHead();
+  let row = thead.insertRow();
+  for (key of data) {
+    let th = document.createElement("th");
+    let text = document.createTextNode(key);
+    th.appendChild(text);
+    row.appendChild(th);
+  }
+}
+
+/*
+This function should manage the delay in building each row. 
+
+*/
+const delay = (rowsToBuild, Winners, theContainer, nextHugo) => {
+  if (typeof nextHugo === "undefined") {
+     var nextHugo = 0;
+    }
+    if (rowsToBuild === 0) {
+      console.log("end");
+    } else {
+      setTimeout( ()=>{
+
+        
+        rowBuilder(Winners[nextHugo], theContainer);
+        rowsToBuild--;
+        nextHugo++;
+        delay(rowsToBuild, Winners, theContainer, nextHugo)
+      }, 1000)
+  }
 }
 
 const each = ( hugos ) => { 
-  const theContainer = document.querySelector('#outcome');
+  let theContainer = document.querySelector('#outcome');
   for ( const novels in hugos ) {
-    let theWinners = hugos[novels];
-    for (thisYear in theWinners ) { 
-      rowBuilder(theWinners[thisYear], theContainer);
-     // (function () {setInterval(rowBuilder, 500, theWinners[thisYear], theContainer);})()
-      // setInterval(rowBuilder, 5000, thisYear, theWinners, theContainer);
-    }
-
-    // for (var x = 0, ln = list.length; x < ln; x++) {
-    //   setTimeout(function(y) {    
-    //     console.log("%d => %d", y, list[y] += 10);
-    //   }, x * 500, x); // we're passing x
-    // // }
-
+    const theWinners = hugos[novels];
+    let rowsToBuild = theWinners.length;
+    delay(rowsToBuild, theWinners, theContainer)
    }
 }
