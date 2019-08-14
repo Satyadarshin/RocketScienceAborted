2f6a276d09d05087f5514adad117b376d769d2e9
Satyadarshin Perry <s.Dog@Satyadarshin.com>
2019-08-14 19:26:19 +0100

    App chugs over one of the two data sets avaiable and presents them in a table. now ready to combine the two data files and present as a combined output table.

--- app/js/app.js
+++ app/js/app.js
@@ -1,80 +1,90 @@
-/* */
+//TODO can I get this array to self-populate by reading the ./data directory?
+// const dataSource = [ "data/hugo_award_novels", "data/nebula_award_novels" ];
+const dataSource = "data/hugo_award_novels";
+ 
 const xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      const response = JSON.parse(xhttp.responseText);
+    if ( this.readyState == 4 && this.status == 200 ) {
+      const response = JSON.parse( xhttp.responseText );
+      //Original function processes just one data source
       each( response );
+      //New function combines multiple data sources first
+      //combineDataSources( response )
     }
-};
-xhttp.open("GET", "data/hugo_award_novels.json", true);
-xhttp.send();
+  }
+    xhttp.open( "GET", dataSource + ".json", true );
+    xhttp.send();
 
-const rowBuilder = (i, tableContainer) => {
-  let winnerRow = document.createElement('tr');
-  let winnerYear = document.createElement('td');
+
+// for ( i = 0; i < dataSource.length; i++ ) {
+//   // console.log( `test: ${dataSource[i]}` );
+//   xhttp.open( "GET", dataSource[i] + ".json", true );
+//   xhttp.send();
+// }
+
+const combineDataSources = ( theAward ) => {
+ console.log( theAward );
+}
+
+const rowBuilder = ( i, tableContainer ) => {
+  let winnerRow = document.createElement( 'tr' );
+  let winnerYear = document.createElement( 'td' );
   winnerYear.textContent = `${i.year}`;
-  let winnerAuthor = document.createElement('td');
+  let winnerAuthor = document.createElement( 'td' );
   winnerAuthor.textContent = `${i.winner.author}`;
-  let winnerTitle = document.createElement('td');
+  let winnerTitle = document.createElement( 'td' );
   winnerTitle.textContent = `${i.winner.title}`;
-  winnerRow.appendChild(winnerYear);
-  winnerRow.appendChild(winnerAuthor);
-  winnerRow.appendChild(winnerTitle);
-  tableContainer.appendChild(winnerRow);
-  
-  setTimeout( ()=>{ 
-    winnerRow.classList.add('test_class');
-    setTimeout( ()=>{ 
-      tableContainer.removeChild(winnerRow)
-    }, 1000) }, 1000)
-  // tableContainer.removeChild(winnerRow);
+  winnerRow.appendChild( winnerYear );
+  winnerRow.appendChild( winnerAuthor );
+  winnerRow.appendChild( winnerTitle );
+  tableContainer.appendChild( winnerRow );
+  setTimeout( () => { 
+    winnerRow.classList.add( "swoosh" );
+    setTimeout( () => { 
+      tableContainer.removeChild( winnerRow )
+    }, 1000) }, 1000 )
 }
 
-//This function is on the backburner. 
-//It should be called once, before the function to chug over the rows 
+//Dynamically build a table header.
 const generateTableHead = ( theContainer, Winners ) => {
-  let thead = theContainer.createTHead();
-  let columnTitle = Object.keys(Winners[0]);
-  for (title in columnTitle) {
-    if (title < 2) {
-     // console.log (columnTitle[title])
-      let th = document.createElement("th");
-      if ( title == 1 ) { 
-        th.setAttribute("colspan", "2");
+  let tableHead = theContainer.createTHead();
+  let columnTitle = Object.keys( Winners[0] );
+  for ( theColumn in columnTitle ) {
+    if ( theColumn < 2 ) {
+      let headerElement = document.createElement( "th" );
+      if ( theColumn == 1 ) { 
+        headerElement.setAttribute( "colspan", "2" );
       }
-      let text = document.createTextNode(columnTitle[title]);
-      th.appendChild(text);
-      thead.appendChild(th);
+      let headerText = document.createTextNode( columnTitle[theColumn] );
+      headerElement.appendChild( headerText );
+      tableHead.appendChild( headerElement );
     }
   }
 }
 
-/*
-This function should manage the delay in building each row. 
-
-*/
-const delay = (rowsToBuild, Winners, theContainer, nextHugo) => {
-  if (typeof nextHugo === "undefined") {
+//This function should manage the delay in building each row. 
+const delay = ( rowsToBuild, Winners, theContainer, nextHugo ) => {
+  if ( typeof nextHugo === "undefined" ) {
     var nextHugo = 0;
-    generateTableHead(theContainer, Winners)
+    generateTableHead( theContainer, Winners )
   }
-  if (rowsToBuild === 0) {
-    console.log("End of row building.");
+  if ( rowsToBuild === 0 ) {
+    console.log( "End of row building." );
   } else {
     setTimeout( ()=> {
-      rowBuilder(Winners[nextHugo], theContainer);
+      rowBuilder( Winners[nextHugo], theContainer );
       rowsToBuild--;
       nextHugo++;
-      delay(rowsToBuild, Winners, theContainer, nextHugo)
+      delay( rowsToBuild, Winners, theContainer, nextHugo )
     }, 1000);
   }
 }
 
 const each = ( hugos ) => { 
-  let theContainer = document.querySelector('#outcome');
+  let theContainer = document.querySelector( "#outcome" );
   for ( const novels in hugos ) {
     const theWinners = hugos[novels];
     let rowsToBuild = theWinners.length;
-    delay(rowsToBuild, theWinners, theContainer);
+    delay( rowsToBuild, theWinners, theContainer );
    }
 }
