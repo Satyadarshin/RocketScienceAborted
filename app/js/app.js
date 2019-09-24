9aa6e45566ce2e14b260c2a79f9f0faef833df47
Satyadarshin Perry <s.Dog@Satyadarshin.com>
2019-09-24 12:05:19 +0100

    Blocked in buttons to control flow of rows: pause, previous, next

--- app/js/app.js
+++ app/js/app.js
@@ -21,18 +21,48 @@ const chooseAward = ( selectedAward ) => {
 const theNebulas = document.querySelector( ".nebula_best_novel" ).addEventListener( 'click', () => { chooseAward( "nebula_award_novels" ) });
 const theHugos = document.querySelector( ".hugo_best_novel" ).addEventListener( 'click', () => { chooseAward( "hugo_award_novels" ) });
 
-const rowBuilder = ( i, tableContainer ) => {
+const rowBuilder = ( thisWinner, tableContainer, thisIndex ) => {
+  const paused = false;
   let winnerRow = document.createElement( 'tr' );
+  //this data- value added so that we can pull out this row separately by array index
+  winnerRow.setAttribute('data-index', thisIndex);
   let winnerYear = document.createElement( 'td' );
-  winnerYear.textContent = `${i.year}`;
+  winnerYear.textContent = `${thisWinner.year}`;
   let winnerAuthor = document.createElement( 'td' );
-  winnerAuthor.textContent = `${i.winner.author}`;
+  winnerAuthor.textContent = `${thisWinner.winner.author}`;
   let winnerTitle = document.createElement( 'td' );
-  winnerTitle.textContent = `${i.winner.title}`;
+  
+  let controls = document.createElement( 'td' );
+  const previousWinner = document.createElement( 'button' );
+  previousWinner.setAttribute( 'class', 'previous_row' );
+  //TODO set up a control so that the next value can't be below 0 
+  previousWinner.setAttribute( 'data-previous', thisIndex-1 );
+  previousWinner.textContent = 'Previous';
+
+  const nextWinner = document.createElement( 'button' );
+  nextWinner.setAttribute( 'class', 'next_row' );
+  //TODO set up a control so the next value can't be greater than the total number of winners
+  nextWinner.setAttribute( 'data-next', thisIndex+1 );
+  nextWinner.textContent = 'next';
+
+  let pauseButton = document.createElement( 'button' );
+  pauseButton.setAttribute('class', 'pause_row');
+  if ( paused ) {
+    pauseButton.setAttribute('class', 'paused');
+  }
+  pauseButton.textContent = 'Pause';
+  controls.appendChild( previousWinner );
+  controls.appendChild( pauseButton );
+  controls.appendChild( nextWinner );
+
+  
+  
+  winnerTitle.textContent = `${thisWinner.winner.title}`;
   winnerRow.appendChild( winnerYear );
   winnerRow.appendChild( winnerAuthor );
   winnerRow.appendChild( winnerTitle );
   tableContainer.appendChild( winnerRow );
+  winnerRow.appendChild( controls);
   setTimeout( () => { 
     winnerRow.classList.add( "swoosh" );
     setTimeout( () => { 
@@ -67,9 +97,10 @@ const delay = ( rowsToBuild, Winners, theContainer, nextHugo ) => {
     console.log( "End of row building." );
   } else {
     setTimeout( ()=> {
-      rowBuilder( Winners[nextHugo], theContainer );
+      rowBuilder( Winners[nextHugo], theContainer, nextHugo );
       rowsToBuild--;
       nextHugo++;
+      //console.log(`the next index is ${nextHugo}`);
       delay( rowsToBuild, Winners, theContainer, nextHugo )
     }, 1000);
   }
@@ -79,8 +110,6 @@ const each = ( hugos ) => {
   //todo: hoist this variable out because it's searched for twice
   let theContainer = document.querySelector( "#outcome" );
   for ( const novels in hugos ) {
-    // console.log( novels)
-    console.log(hugos[novels]);
     const theWinners = hugos[novels];
     let rowsToBuild = theWinners.length;
     delay( rowsToBuild, theWinners, theContainer );
